91fd0dd5ac87bb06bd536d59b03a98343fa419af
dimkaar <dimkaar1102@gmail.com>
2019-03-04 23:10:20 +0300

    feat(src): added functions to work with time and duration count, put the duration time into points

--- src/points.js
+++ src/points.js
@@ -9,7 +9,7 @@ const createPoint = (data) => {
           <h3 class="trip-point__title">${data.text}</h3>
           <p class="trip-point__schedule">
             <span class="trip-point__timetable">${data.time}</span>
-            <span class="trip-point__duration"></span>
+            <span class="trip-point__duration">${data.duration}</span>
           </p>
           <p class="trip-point__price">${data.price}&euro;</p>
           <ul class="trip-point__offers">
@@ -25,13 +25,38 @@ export const renderPoints = (amount) => {
   let tmpData;
 
   const parseData = (data) => {
+    let time;
+
+    const getPointTime = (date) => {
+      return new Date(date).getHours() + Units.startUnit + `:` + new Date(date).getMinutes() + `–` + getRandomIntegerFromInterval(0, Units.hoursInDay) + `:` + getRandomIntegerFromInterval(new Date(date).getMinutes(), (Units.minutesInHour - Units.startUnit));
+    };
+
+    const replaceText = (str, search, replacement) => {
+      return str.split(search).join(replacement);
+    };
+
+    time = getPointTime(data.date);
+
+    if (~time.indexOf(`24`)) {
+      time = replaceText(time, `24`, `00`);
+    }
 
     const getRandomType = (typesObj) => {
       return Object.keys(typesObj)[getRandomIntegerFromInterval(0, Object.keys(typesObj).length - 1)];
     };
 
-    const getPointTime = (date) => {
-      return new Date(date).getHours() + Units.startUnit + `:` + new Date(date).getMinutes() + `–` + getRandomIntegerFromInterval(new Date(date).getHours(), Units.hoursInDay - 1) + `:` + getRandomIntegerFromInterval(new Date(date).getMinutes(), Units.minutesInHour);
+    const getDateFromStr = (string) => new Date(0, 0,0, string.split(`:`)[0], string.split(`:`)[1]);
+
+    const countDuration = () => {
+      const firstTime = time.split(`–`)[0];
+      const secondTime = time.split(`–`)[time.split(`–`).length - 1];
+
+      let difference = (getDateFromStr(secondTime) - getDateFromStr(firstTime));
+
+      let hours = Math.floor((difference % 86400000) / 3600000);
+      let minutes = Math.round(((difference % 86400000) % 3600000) / 60000);
+      let result = hours + `h ` + minutes + `m`;
+      return result;
     };
 
     return {
@@ -41,7 +66,8 @@ export const renderPoints = (amount) => {
       offers: data.offers,
       text: data.text,
       date: new Date(data.date),
-      time: getPointTime(data.date),
+      time,
+      duration: countDuration(),
       price: data.price
     };
   };
